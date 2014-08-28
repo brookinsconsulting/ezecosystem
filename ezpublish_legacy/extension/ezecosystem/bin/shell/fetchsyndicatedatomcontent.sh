d0095054b713d9bbe8cf8c45739258faaaa07647
Brookins Consulting <info@brookinsconsulting.com>
2014-08-28 16:28:09 -0700

    Updated: Added addition atom feeds to import ini and fetch shell script

--- ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
@@ -4,8 +4,6 @@ php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --sourc
 
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=brookinsconsultinggithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezoegithubatomimporthandler -vdc;
-
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=gggeekgithubatomimporthandler -vdc;
 
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezoegithubatomimporthandler -vdc;
@@ -16,6 +14,14 @@ php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --sourc
 
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=crevillogithubatomimporthandler -vdc;
 
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=andreromgithubatomimporthandler -vdc;
+
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=dpobelgithubatomimporthandler -vdc;
+
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=bdunogiergithubatomimporthandler -vdc;
+
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=pedroresendegithubatomimporthandler -vdc;
+
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublish-communitygithubatomimporthandler -vdc;
 
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublish-kernelgithubatomimporthandler -vdc;
@@ -25,5 +31,5 @@ php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --sourc
 php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezcommunityjiraatomimporthandler -vdc;
 
 ### php ./clr --clear-all;
-php ./clr --clear-tag=content;
+### php ./clr --clear-tag=content;
 
