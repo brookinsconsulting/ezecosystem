d9544e6d09a2d51351707ad76ae6fecb056d78ba
Brookins Consulting <info@brookinsconsulting.com>
2014-09-07 04:19:14 -0700

    Added: static cache settings override to configure the static cache feature

---
+++ ezpublish_legacy/settings/override/staticcache.ini.append.php
@@ -0,0 +1,42 @@
+#?ini charset="utf-8"?
+# eZ Publish configuration file.
+#
+# NOTE: It is not recommended to edit this file directly, instead
+#       a file in override should be created for setting the
+#       values that is required for your site. Either create
+#       a file called settings/override/staticcache.ini.append or
+#       settings/override/staticcache.ini.append.php for more security
+#       in non-virtualhost modes (the .php file may already be present
+#       and can be used for this purpose).
+[CacheSettings]
+# This setting has been deprecated since version 4.4
+# Hostname is read from site.ini.[SiteSettings].SiteURL per siteaccess
+# defined in staticcache.ini.[CacheSettings].CachedSiteAccesses
+# This setting will be removed in future release
+HostName=
+StaticStorageDir=static
+MaxCacheDepth=4
+
+# A list of url's to cache. You can use the * wildcard to include a whole
+# subtree.
+CachedURLArray[]=/
+CachedURLArray[]=/Mirror*
+CachedURLArray[]=/About*
+CachedURLArray[]=/About-eZ-Publish*
+CachedURLArray[]=/Contact-Us*
+
+# A list of site accesses to generate static content for
+CachedSiteAccesses[]
+CachedSiteAccesses[]=ezwebin_site_user
+
+# A list of locations that will be updated whenever an object is published. You
+# can NOT use a wildcard here.
+AlwaysUpdateArray[]
+AlwaysUpdateArray[]=/
+
+# Defer cache generation to cronjob.
+CronjobCacheClear=disabled
+
+# Controls whenever <!-- Generated: YYY-MM-DD HH:MM:SS --> comment
+# should be appended into the generated static cache file.
+AppendGeneratedTime=true
