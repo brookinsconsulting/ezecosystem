301906ce0aac911608854d2b387699fd3fb5b582
Brookins Consulting <info@brookinsconsulting.com>
2014-09-12 06:36:12 -0500

    Updated: Added optional siteaccess script argument

--- ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublishjiraatomimporthandler -vdc;
+if [[ -z "$1" ]]; then
+siteaccess="ezwebin_site_user";
+else
+siteaccess="$1";
+fi
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezcommunityjiraatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublishjiraatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublish-legacygithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezcommunityjiraatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublish-communitygithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-legacygithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=ezpublish-kernelgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-communitygithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=brookinsconsultinggithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-kernelgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=brookinsconsultinggistgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=brookinsconsultinggithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=gggeekgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=brookinsconsultinggistgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=jdespatisgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=gggeekgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=crevillogithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=jdespatisgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=andreromgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=crevillogithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=dpobelgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=andreromgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=bdunogiergithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=dpobelgithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=pedroresendegithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=bdunogiergithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=fabienpotencieratomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=pedroresendegithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=yannickrogergithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=fabienpotencieratomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=lolautruchegithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=yannickrogergithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=peterkeunggithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=lolautruchegithubatomimporthandler -vdc;
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s ezwebin_site_user --source-handlers=xrowgithubatomimporthandler -vdc;
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=peterkeunggithubatomimporthandler -vdc;
+
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=xrowgithubatomimporthandler -vdc;
 
