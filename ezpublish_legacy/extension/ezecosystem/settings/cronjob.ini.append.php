140a4af28645a3abace2d35b5a4146e659704073
Brookins Consulting <info@brookinsconsulting.com>
2014-09-12 08:26:34 -0500

    Added: Added updateviewcount cronjob configuration settings configured for production

--- ezpublish_legacy/extension/ezecosystem/settings/cronjob.ini.append.php
+++ ezpublish_legacy/extension/ezecosystem/settings/cronjob.ini.append.php
@@ -1,4 +1,19 @@
+<?php /* #?ini charset="utf-8"?
 
 [CronjobPart-rssimport]
 Scripts[]=rssimport.php
+
+[CronjobPart-updateviewcount]
+Scripts[]=updateviewcount.php
+
+[CronjobPart-staticcache_cleanup]
+Scripts[]=staticcache_cleanup.php
+
+[CronjobSettings]
+Scripts[]=staticcache_cleanup.php
+Scripts[]=updateviewcount.php
+
+*/ ?>
+
+
 
