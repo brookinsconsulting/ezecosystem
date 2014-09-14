f4fb38ddebdd5b9444e67a5d3d52c7b30231efa6
Brookins Consulting <info@brookinsconsulting.com>
2014-09-13 22:04:00 -0500

    Updated: Added indexcontent cronjob to crontab. Added staticcache_cleanup cronjob to crontab. Enabled sqliimport_cleanup cronjob.

--- ezpublish_legacy/extension/ezecosystem/settings/cronjob.ini.append.php
+++ ezpublish_legacy/extension/ezecosystem/settings/cronjob.ini.append.php
@@ -9,9 +9,14 @@ Scripts[]=updateviewcount.php
 [CronjobPart-staticcache_cleanup]
 Scripts[]=staticcache_cleanup.php
 
+[CronjobPart-indexcontent]
+Scripts[]=indexcontent.php
+
 [CronjobSettings]
 Scripts[]=staticcache_cleanup.php
 Scripts[]=updateviewcount.php
+Scripts[]=indexcontent.php
+Scripts[]=rssimport.php
 
 */ ?>
 
