28d9cd365f66e439e4a66263bdb627a3bfc508d5
Brookins Consulting <info@brookinsconsulting.com>
2014-09-13 19:20:14 -0500

    Added: Added override and siteaccess site.ini.append.php settings files

---
+++ ezpublish_legacy/settings/siteaccess/ezwebin_site_admin/site.ini.append.php
@@ -0,0 +1,76 @@
+<?php /* #?ini charset="utf-8"?
+
+[DebugSettings]
+DebugOutput=enabled
+
+# If you disable some modules or part of a module you can use the
+# SiteAccessRules group, this defines a list of rules which are
+# run in order.
+#
+# The following is an example of how to disable content/search
+# and the rss module
+
+[SiteAccessRules]
+Rules[]
+# Set access policy to allowed
+Rules[]=access;enable
+# Special syntax which means any module
+# This means that for now all modules are enabled
+Rules[]=moduleall
+# Set policy to denied
+Rules[]=access;disable
+Rules[]=module;user/register
+Rules[]=module;content/versionview
+Rules[]=module;content/history
+Rules[]=module;content/tipafriend
+
+[InformationCollectionSettings]
+EmailReceiver=info@brookinsconsulting.com
+
+[Session]
+SessionNamePerSiteAccess=disabled
+
+[SiteSettings]
+SiteName=eZ Ecosystem
+SiteURL=ezecosystem.org
+LoginPage=embedded
+AdditionalLoginFormActionURL=http://admin.ezecosystem.org/user/login
+
+[UserSettings]
+RegistrationEmail=info@brookinsconsulting.com
+
+[SiteAccessSettings]
+RequireUserLogin=false
+RelatedSiteAccessList[]
+RelatedSiteAccessList[]=ezwebin_site_user
+RelatedSiteAccessList[]=ezwebin_site_admin
+RelatedSiteAccessList[]=eng
+ShowHiddenNodes=false
+
+[DesignSettings]
+SiteDesign=eze
+AdditionalSiteDesignList[]
+AdditionalSiteDesignList[]=eze
+AdditionalSiteDesignList[]=ezwebin
+AdditionalSiteDesignList[]=base
+
+[RegionalSettings]
+Locale=eng-US
+ContentObjectLocale=eng-US
+ShowUntranslatedObjects=disabled
+SiteLanguageList[]
+SiteLanguageList[]=eng-US
+TextTranslation=disabled
+
+[FileSettings]
+VarDir=var/ezwebin_site
+
+[ContentSettings]
+TranslationList=
+
+[MailSettings]
+AdminEmail=info@brookinsconsulting.com
+EmailSender=info@ezecosystem.org
+
+
+*/ ?>
