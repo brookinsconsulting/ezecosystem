d62e3340921ab8963bab13dc03f33352dbd10d02
Brookins Consulting <info@brookinsconsulting.com>
2014-09-15 00:08:58 -0500

    Updated: Simplified script internals to reduce number of script calls required to maintain. Maintained same functionality

--- ezpublish_legacy/extension/ezecosystem/bin/shell/generatestaticcacheindexes.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/generatestaticcacheindexes.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
+# Test for optional siteacess shell argument #1
 if [[ -z "$1" ]]; then
+# Refresh using the default ezpublish siteacces name
 siteaccess="ezwebin_site_user";
 else
+# Refresh using the optional siteaccess shell argument
 siteaccess="$1";
 fi
 
+# Alert the user of the siteaccess being used
 echo "Using siteaccess $siteaccess";
 echo "";
 
+# Test for optional uri shell argument #2
 if [[ -z "$2" ]]; then
-type="index";
+# Refresh Home Page Indexes
+uri="/";
 else
-type="$2";
+uri="$2";
 fi
 
+# Test for optional subtree shell argument #3
 if [[ -z "$3" ]]; then
+# Refresh indexes only
 subtree="";
+children=" --children ";
 echo "Caching indexes only";
 echo "";
 else
+# Refresh subtree indexes only
 echo "Caching subtree with *";
 echo "";
 subtree="$3";
+children=" ";
 fi
 
-if [[ "$type" = "index" ]]; then
-echo "Generating indexes ...";
+# Alert the user of the static cache generation starting
+echo "Generating $uri indexes ...";
 echo "";
 
-# Refresh Home Page Indexes
-
-php ./extension/bcgeneratestaticcache/bin/php/bcgeneratestaticcache.php --subtree=/$subtree --children --force -s $siteaccess;
-
-elif [[ "$type" = "blogindex" ]]; then
-echo "Generating blog indexes ...";
-echo "";
-
-# Refresh Mirror Blog Indexes
-
-php ./extension/bcgeneratestaticcache/bin/php/bcgeneratestaticcache.php --subtree=/Mirror$subtree --children --force -s $siteaccess;
-
-fi
+# Refresh Static Cache
+php ./extension/bcgeneratestaticcache/bin/php/bcgeneratestaticcache.php --subtree=$uri$subtree$children--force -s $siteaccess;
 
 exit;
 
