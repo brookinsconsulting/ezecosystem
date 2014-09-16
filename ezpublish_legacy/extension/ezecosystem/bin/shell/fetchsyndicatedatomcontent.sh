f0f5c0d1c97e2021279284caff5d3d27d680d14d
Brookins Consulting <info@brookinsconsulting.com>
2014-09-15 21:47:13 -0500

    Updated: Refactor fetchsyndicatedatomcontent.sh shell helper script to iterate over array of handler sources instead of using individual commands. Maintained existing functionality. Added sqliimport_cleanup cronjob to script since it is needed to clear view cache for imported items.

--- ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
 
+# Define import source handler arrays
+atom_jira_sources=( ezpublishjiraatomimporthandler ezcommunityjiraatomimporthandler )
+atom_github_sources=( ezpublish-legacygithubatomimporthandler ezpublish-communitygithubatomimporthandler ezpublish-kernelgithubatomimporthandler brookinsconsultinggithubatomimporthandler brookinsconsultinggistgithubatomimporthandler gggeekgithubatomimporthandler jdespatisgithubatomimporthandler crevillogithubatomimporthandler andreromgithubatomimporthandler dpobelgithubatomimporthandler bdunogiergithubatomimporthandler pedroresendegithubatomimporthandler fabienpotencieratomimporthandler yannickrogergithubatomimporthandler lolautruchegithubatomimporthandler peterkeunggithubatomimporthandler xrowgithubatomimporthandler )
+
+# Test for optional siteacecss shell argument
 if [[ -z "$1" ]]; then
 siteaccess="ezwebin_site_user";
 else
 siteaccess="$1";
 fi
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublishjiraatomimporthandler -vdc;
+# Iterate throug jira sources and run atom content import
+for source in "${atom_jira_sources[@]}"
+do
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=$source;
+done
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezcommunityjiraatomimporthandler -vdc;
+# Iterate throug github sources and run atom content import
+for source in "${atom_github_sources[@]}"
+do
+php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=$source;
+done
 
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-legacygithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-communitygithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=ezpublish-kernelgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=brookinsconsultinggithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=brookinsconsultinggistgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=gggeekgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=jdespatisgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=crevillogithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=andreromgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=dpobelgithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=bdunogiergithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=pedroresendegithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=fabienpotencieratomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=yannickrogergithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=lolautruchegithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=peterkeunggithubatomimporthandler -vdc;
-
-php ./extension/sqliimport/bin/php/sqlidoimport.php -s $siteaccess --source-handlers=xrowgithubatomimporthandler -vdc;
-
+# Clear view cache for all importated content
+php -d memory_limit=-1 ./runcronjobs.php -s $siteaccess sqliimport_cleanup;
