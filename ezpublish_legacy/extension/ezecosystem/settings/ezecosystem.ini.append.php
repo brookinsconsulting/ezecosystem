3b5870b4d247731c712ed9e9df2bf45adba954db
Brookins Consulting <info@brookinsconsulting.com>
2014-09-15 21:23:06 -0500

    Added: ezecosystem.ini default settings and project settings override

---
+++ ezpublish_legacy/extension/ezecosystem/settings/ezecosystem.ini.append.php
@@ -0,0 +1,64 @@
+<?php /* #?ini charset="utf-8"?
+
+[eZecosystemSettings]
+ViewCountDisplay=enabled
+ViewCountThreshold=2
+
+[NodeIDSettings]
+BlogsNodeID=216
+IssuesNodeID=9281
+CommunityIssuesNodeID=4198
+NotificationNodeID=14353
+SidebarCommunityNodeID=969
+SidebarPlanetariumNodeID=970
+
+[ClassIdentifierSettings]
+classBlog=blog
+classBlogPost=blog_post
+classIssue=issue
+classIssuePost=issue_post
+classNotification=notification
+
+[AttributeIdentifierSettings]
+blogPostPublicationDate=blog_post/publication_date
+issuePostPublicationDate=issue_post/publication_date
+
+[PopularSidebarSettings]
+ClassIDs[]
+ClassIDs[]=20
+ClassIDs[]=45
+
+[HomePageFetchSettings]
+ClassIdentifiers[]
+ClassIdentifiers[]=blog_post
+
+[TagSidebarSettings]
+TagCloudLimit=140
+TagCloudClassIdentifier=blog_post
+TagCloudExcludeTags[]
+TagCloudExcludeTags[]=greg@granitehorizon.com (Greg McAvoy-Jensen)
+TagCloudExcludeTags[]=greg@granitehorizon.com (Robert Rose)
+TagCloudExcludeTags[]=ranitehorizon
+TagCloudExcludeTags[]=01 May 2008
+TagCloudExcludeTags[]=08 Mar 2011
+TagCloudExcludeTags[]=04 Nov 2009
+TagCloudExcludeTags[]=25 Aug 2009
+TagCloudExcludeTags[]=30 Sep 2010
+TagCloudExcludeTags[]=Thu
+TagCloudExcludeTags[]=çåååä
+TagCloudExcludeTags[]=wascou
+TagCloudExcludeTagStrings[]
+TagCloudExcludeTagStrings[]=EZP-
+TagCloudExcludeTagStrings[]=COM-
+
+[TwitterSidebarSettings]
+TwitterSearchUrl=https://twitter.com/search?q=ezpublish+OR+ezcommunity+OR+ezfind+OR+ezsystems+OR+ezsummer
+TwitterSearchTitle=Tweets about "ezpublish OR ezcommunity OR ezfind OR ezsystems"
+
+[FeedSidebarSettings]
+MainRssUrl=/rss/feed/rss
+MainRssTitle=eZecosystem RSS Feed
+MainOpmlUrl=https://raw.github.com/brookinsconsulting/ezecosystemopml/master/index.opml
+MainOpmlTitle=eZecosystem OPML File
+
+*/ ?>
