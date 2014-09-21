3009bc8a769940fc3675e720dd1bdfbd577b6202
Brookins Consulting <info@brookinsconsulting.com>
2014-09-20 22:34:24 -0500

    Added: Added shell script to refresh home page static cache

---
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/refreshhomepagestaticcache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Test for optional siteacecss shell argument
+if [[ -z "$1" ]]; then
+siteaccess="ezwebin_site_user";
+else
+siteaccess="$1";
+fi
+
+# Refresh static cache of the root node and it's children
+./extension/ezecosystem/bin/shell/generatestaticcacheindexes.sh $1 /;
+
+# Refresh static cache of the /Mirror node and it's children
+./extension/ezecosystem/bin/shell/generatestaticcacheindexes.sh $1 /Mirror;
+
+exit;
