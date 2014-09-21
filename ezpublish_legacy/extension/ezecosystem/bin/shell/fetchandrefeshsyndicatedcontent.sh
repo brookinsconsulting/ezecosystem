af8a183f0da25396aa7e74e7945008f36c38b92a
Brookins Consulting <info@brookinsconsulting.com>
2014-09-20 22:36:41 -0500

    Added: Added shell script to import import syndicated content and refresh static cache

---
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchandrefeshsyndicatedcontent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Test for optional siteacecss shell argument
+if [[ -z "$1" ]]; then
+siteaccess="ezwebin_site_user";
+else
+siteaccess="$1";
+fi
+
+# Fetch all syndicated content atom and rss
+./extension/ezecosystem/bin/shell/fetchsyndicatedcontent.sh $1;
+
+# Refresh home page(s) static cache
+./extension/ezecosystem/bin/shell/refreshhomepagestaticcache.sh $1;
+
+exit;
