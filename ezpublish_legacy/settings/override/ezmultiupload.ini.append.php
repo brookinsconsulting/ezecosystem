4984e779a63666684221697bebd3dd49393c6115
Brookins Consulting <info@brookinsconsulting.com>
2014-09-27 03:38:22 -0500

    Added: Added configuration settings customized for binary file uplaods

---
+++ ezpublish_legacy/settings/override/ezmultiupload.ini.append.php
@@ -0,0 +1,29 @@
+<?php /*
+
+[MultiUploadSettings]
+AvailableClasses[]
+AvailableClasses[]=folder
+AvailableClasses[]=gallery
+
+AvailableSubtreeNode[]
+
+MultiuploadHandlers[]
+
+[FileTypeSettings_folder]
+FileType[]
+FileType[]=tar.gz
+FileType[]=zip
+FileType[]=tar.bz2
+FileType[]=*.*
+
+[FileTypeSettings_gallery]
+FileType[]
+FileType[]=*.jpg
+FileType[]=*.png
+FileType[]=*.gif
+FileType[]=*.flv
+FileType[]=*.mp4
+FileType[]=*.mov
+FileType[]=*.JPG
+
+*/ ?>
