1892d0d6b01cb6d388f13801751ced228172c052
Brookins Consulting <info@brookinsconsulting.com>
2014-09-30 07:34:21 -0500

    Updated: Added code to scripts to only clear static cache on calls to script where siteaccess is not production siteaccess

--- ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedatomcontent.sh
@@ -26,7 +26,10 @@ done
 # Clear view cache for all importated content
 php -d memory_limit=-1 ./runcronjobs.php --siteaccess $siteaccess sqliimport_cleanup;
 
+# Test to determin if script is not being used on production siteacess
+if [[ $siteaccess != "ezwebin_site_user" ]]; then
 # Refresh static cache for all importated content
 php -d memory_limit=-1 ./runcronjobs.php --siteaccess $siteaccess staticcache_cleanup;
+fi
 
 exit;
--- ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedrsscontent.sh
+++ ezpublish_legacy/extension/ezecosystem/bin/shell/fetchsyndicatedrsscontent.sh
@@ -8,6 +8,10 @@ fi
 
 php ./runcronjobs.php --siteaccess $siteaccess ezerssimport;
 
-php ./runcronjobs.php --siteaccess $siteaccess staticcache_cleanup;
+# Test to determin if script is not being used on production siteacess
+if [[ $siteaccess != "ezwebin_site_user" ]]; then
+# Refresh static cache for all importated content
+php -d memory_limit=-1 ./runcronjobs.php --siteaccess $siteaccess staticcache_cleanup;
+fi
 
 exit;
