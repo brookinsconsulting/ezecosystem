bd9de742bcddcb7228943eaabf9124c36c894256
Brookins Consulting <info@brookinsconsulting.com>
2014-10-23 23:13:20 -0500

    Removed: Removed unused siteaccess configuration

--- ezpublish_legacy/settings/siteaccess/ezdemo_site_admin/content.ini.append.php
+++
@@ -1,8 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[VersionView]
-AvailableSiteDesignList[]
-AvailableSiteDesignList[]=ezdemo_site
-AvailableSiteDesignList[]=admin2
-AvailableSiteDesignList[]=admin
-*/ ?>
