e034a913dbbe3a8ed24d49d1b8675256f3559335
Brookins Consulting <info@brookinsconsulting.com>
2017-01-09 13:56:16 -0600

    Update: Configure eZFind Service Settings

---
+++ ezpublish_legacy/settings/override/ezfind.ini.append.php
@@ -0,0 +1,14 @@
+<? /*
+
+[LanguageSearch]
+SearchMainLanguageOnly=disabled
+MultiCore=enabled
+DefaultCore=ezecosystem_eng
+LanguagesCoresMap[]
+LanguagesCoresMap[eng-US]=ezecosystem_eng
+
+[IndexOptions]
+DisableDeleteCommits=true
+OptimizeOnCommit=disabled
+
+*/ ?>
---
+++ ezpublish_legacy/settings/override/solr.ini.append.php
@@ -0,0 +1,11 @@
+<?php
+
+[SolrBase]
+SearchServerURI=http://ef:8990/solr
+ConnectionTimeout=10
+ProcessTimeout=20
+
+Shards[]
+Shards[ezecosystem_eng]=http://ef:8990/solr/ezecosystem_eng
+
+?>
